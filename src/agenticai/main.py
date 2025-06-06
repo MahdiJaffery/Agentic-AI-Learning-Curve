c6ccc23115df3ed96a538ff29a858158a7c8d161
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-06-06 22:46:10 +0500

    Added Agents

--- src/agenticai/main.py
+++ src/agenticai/main.py
@@ -8,15 +8,7 @@ from agenticai.crew import Agenticai
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
 def run():
-    """
-    Run the crew.
-    """
     inputs = {
         'topic': 'AI LLMs',
         'current_year': str(datetime.now().year)
@@ -29,9 +21,6 @@ def run():
 
 
 def train():
-    """
-    Train the crew for a given number of iterations.
-    """
     inputs = {
         "topic": "AI LLMs",
         'current_year': str(datetime.now().year)
@@ -43,9 +32,6 @@ def train():
         raise Exception(f"An error occurred while training the crew: {e}")
 
 def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
     try:
         Agenticai().crew().replay(task_id=sys.argv[1])
 
@@ -53,9 +39,6 @@ def replay():
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
 def test():
-    """
-    Test the crew execution and returns the results.
-    """
     inputs = {
         "topic": "AI LLMs",
         "current_year": str(datetime.now().year)
