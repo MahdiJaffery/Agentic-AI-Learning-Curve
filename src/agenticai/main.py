09e56f776078f93382bc07ffdbe42637cd599211
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-06-06 22:59:08 +0500

    Added User Input for Files/Topic

--- src/agenticai/main.py
+++ src/agenticai/main.py
@@ -4,48 +4,81 @@ import warnings
 
 from datetime import datetime
 
-from agenticai.crew import Agenticai
+from siriil.crew import Siriil
+
+import pandas as pd
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 def run():
+    """
+    Run the crew.
+    """
+    inputType = 'N'
+
+    while inputType not in ['1', '2']:
+        inputType = str(input("Types:\n1.\tTopic\n2.\tFile\nEnter Choice: "))
+        if inputType not in ['1', '2']:
+            print("Invalid Selection!\n")
+
+    topic, filename = '', ''
+
+    if(inputType == '2'):
+        filename = str(input("Enter File Name: "))
+        filename += ".xlsx"
+        df = pd.read_excel(filename, sheet_name="TDSheet")
+    else:
+        topic = str(input("Enter Topic: "))
+        
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'topic': f'{topic}' if topic is not None else f'{df}',
+        'name': 'Lead Analyst',
+        'company_name': 'SiRiiL',
+        'current_year': str(datetime.now().year),
+        # 'current_date': str(datetime.now())
     }
     
     try:
-        Agenticai().crew().kickoff(inputs=inputs)
+        Siriil().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
 def train():
+    """
+    Train the crew for a given number of iterations.
+    """
     inputs = {
         "topic": "AI LLMs",
         'current_year': str(datetime.now().year)
     }
     try:
-        Agenticai().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        Siriil().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
 
 def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
     try:
-        Agenticai().crew().replay(task_id=sys.argv[1])
+        Siriil().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
 def test():
+    """
+    Test the crew execution and returns the results.
+    """
     inputs = {
         "topic": "AI LLMs",
         "current_year": str(datetime.now().year)
     }
     
     try:
-        Agenticai().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        Siriil().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
